f6585495c592b1ac5565c6461cf89720bc6cb68e
rmalaquias <rafael.malaquias@hotmail.com>
2023-09-05 22:52:41 -0300

    Envio aplicação Conversão de temperatura

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int temperatura;
+        int temperatura_convertida;
+
+        System.out.println("Bem vindo ao conversor de temperatura!!!");
+        System.out.println("Qual a temperatura, em graus celsius você deseja converter?");
+
+        temperatura = scanner.nextInt();
+        temperatura_convertida = scanner.nextInt();
+
+
+        int temperaturaFahrenheit = (int) (temperatura * 1.8)+32;
+
+        String mensagem = """
+                Olá, você digitou a temperatura %d ºC, e está temperatura
+                equivale a %d º Fahrenheit!!!
+                """.formatted(temperatura,temperaturaFahrenheit);
+
+        System.out.println(mensagem);
+
+    }
+}
