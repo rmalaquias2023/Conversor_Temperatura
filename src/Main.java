225cce3ad3915f6f028a8c5f8f41252baa8ff7f7
RAFAEL ANTUNES MALAQUIAS <126631387+rmalaquias2023@users.noreply.github.com>
2023-09-05 23:27:48 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int temperatura;
+        double  temperatura;
+        int temperaturaFahrenheit;
 
         System.out.println("Bem vindo ao conversor de temperatura!!!");
         System.out.println("Qual a temperatura, em graus celsius você deseja converter?");
 
-        temperatura = scanner.nextInt();
+        temperatura = scanner.nextDouble();
 
-        int temperaturaFahrenheit = (int) (temperatura * 1.8)+32;
+        temperaturaFahrenheit = (int) (temperatura * 1.8)+32;
+
 
         String mensagem = """
-                Olá, você digitou a temperatura %d ºC, e está temperatura
+                Olá, você digitou a temperatura %.2f ºC, e está temperatura
                 equivale a %d º Fahrenheit!!!
                 """.formatted(temperatura,temperaturaFahrenheit);
 
